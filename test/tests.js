b0050c0729c602ac1ec91fb02745d180269d51a3
Chris Breneman <crispy@cluenet.org>
2015-04-02 10:50:21 -0400

    More common schema

--- test/tests.js
+++ test/tests.js
@@ -3,7 +3,9 @@ let createSchema = require('../lib').createSchema;
 let ValidationError = require('../lib').ValidationError;
 let Mixed = require('../lib').Mixed;
 let or = require('../lib').or;
+let map = require('../lib').map;
 let FieldError = require('../lib').FieldError;
+let _ = require('lodash');
 
 describe('CommonSchema', function() {
 
@@ -339,6 +341,331 @@ describe('CommonSchema', function() {
 			expect(schema.normalize('afoo')).to.equal('afoox');
 			expect( () => schema.normalize('foo') ).to.throw(ValidationError);
 			expect( () => schema.normalize('foo') ).to.throw('Test validator');
+		});
+
+	});
+
+	describe('#validate', function() {
+
+		const schema = createSchema({
+			foo: {
+				bar: String,
+				baz: Number
+			},
+			miss: Date,
+			arr: [ {
+				zip: Date
+			} ],
+			map: map(Number),
+			bin: Buffer,
+			boo: Boolean,
+			mix: Mixed,
+			o: or({}, Number, String, {
+				qux: {
+					type: Number,
+					required: true
+				},
+				bam: String
+			})
+		});
+
+		it('valid', function() {
+			expect(schema.validate({
+				foo: {
+					bar: '8',
+					baz: 8
+				},
+				arr: [
+					{
+						zip: new Date('2014-01-01T00:00:00Z')
+					},
+					{
+						zip: new Date(1427982068722)
+					},
+					{
+						zip: new Date()
+					},
+					{}
+				],
+				map: {
+					foo: 2,
+					bar: 4
+				},
+				bin: new Buffer('YXNkZg==', 'base64'),
+				boo: true,
+				mix: { a: [ function() {} ], b: 5 },
+				o: {
+					qux: 4,
+					bam: '7'
+				}
+			})).to.equal(true);
+		});
+
+		it('invalid', function() {
+			const expectedErrors = [
+				{
+					'field': 'foo.bar',
+					'code': 'invalid_type',
+					'message': 'Must be a string'
+				},
+				{
+					'field': 'foo.baz',
+					'code': 'invalid_type',
+					'message': 'Must be a number'
+				},
+				{
+					'field': 'arr.0.zip',
+					'code': 'invalid_type',
+					'message': 'Must be a date'
+				},
+				{
+					'field': 'arr.1.zip',
+					'code': 'invalid_type',
+					'message': 'Must be a date'
+				},
+				{
+					'field': 'map.bar',
+					'code': 'invalid_type',
+					'message': 'Must be a number'
+				},
+				{
+					'field': 'bin',
+					'code': 'invalid_type',
+					'message': 'Must be a buffer'
+				},
+				{
+					'field': 'boo',
+					'code': 'invalid_type',
+					'message': 'Must be a boolean'
+				},
+				{
+					'field': 'o.qux',
+					'code': 'invalid_type',
+					'message': 'Must be a number'
+				}
+			];
+
+			try {
+				schema.validate({
+					foo: {
+						bar: 8,
+						baz: '8'
+					},
+					arr: [
+						{
+							zip: '2014-01-01T00:00:00Z'
+						},
+						{
+							zip: 1427982068722
+						},
+						{
+							zip: new Date()
+						},
+						{}
+					],
+					map: {
+						foo: 2,
+						bar: '4'
+					},
+					bin: 'YXNkZg==',
+					boo: 'yes',
+					mix: { a: [ function() {} ] },
+					o: {
+						qux: '4',
+						bam: '7'
+					}
+				});
+			} catch (ex) {
+				expect(ex instanceof ValidationError).to.equal(true);
+				expect(ex.data.fieldErrors).to.deep.equal(expectedErrors);
+				return;
+			}
+			throw new Error('Should not reach');
+		});
+
+	});
+
+	describe('#transformAsync', function() {
+
+		it('test1', function(done) {
+			const schema = createSchema([ { foo: String } ]);
+			let obj = [ {}, { a: true } ];
+			schema.transformAsync(obj, {
+				onField(field, value) {
+					return new Promise(function(resolve) {
+						setTimeout(function() {
+							if (_.isPlainObject(value)) {
+								value.addedThing = true;
+							}
+							resolve(value);
+						}, 5);
+					});
+				}
+			}).then(function(result) {
+				expect(result).to.deep.equal([ { addedThing: true }, { addedThing: true, a: true } ]);
+				done();
+			}).catch(done);
+		});
+
+		it('test2', function(done) {
+
+			const schema = createSchema({
+				foo: {
+					bar: String,
+					baz: Number
+				},
+				miss: Date,
+				arr: [ {
+					zip: Date
+				} ],
+				map: map(Number),
+				bin: Buffer,
+				boo: Boolean,
+				mix: Mixed,
+				o: or({}, Number, String, {
+					qux: {
+						type: Number,
+						required: true
+					},
+					bam: String
+				})
+			});
+
+			let obj = schema.normalize({
+				foo: {
+					bar: 8,
+					baz: '8'
+				},
+				arr: [
+					{
+						zip: '2014-01-01T00:00:00Z'
+					},
+					{
+						zip: 1427982068722
+					},
+					{
+						zip: new Date()
+					},
+					{}
+				],
+				map: {
+					foo: 2,
+					bar: '4'
+				},
+				bin: 'YXNkZg==',
+				boo: 'yes',
+				mix: { a: [ function() {} ] },
+				o: {
+					qux: '4',
+					bam: '7'
+				},
+				extraField: 'foo'
+			}, { allowUnknownFields: true });
+
+			schema.transformAsync(obj, {
+
+				onField(field, value) {
+					return new Promise(function(resolve) {
+						setTimeout(function() {
+							if (_.isString(value)) {
+								value = 'a' + value;
+							} else if (_.isNumber(value)) {
+								value = value + 1;
+							} else if (_.isPlainObject(value)) {
+								value.addedThing = true;
+							}
+							resolve(value);
+						}, 5);
+					});
+				},
+
+				onUnknownField(field, value) {
+					return new Promise(function(resolve) {
+						setTimeout(function() {
+							if (_.isString(value)) {
+								value = 'b' + value;
+							} else if (_.isNumber(value)) {
+								value = value + 2;
+							} else if (_.isPlainObject(value)) {
+								value.addedThing2 = true;
+							}
+							resolve(value);
+						}, 5);
+					});
+				},
+
+				postField(field, value) {
+					return new Promise(function(resolve) {
+						setTimeout(function() {
+							if (_.isString(value)) {
+								value = 'c' + value;
+							} else if (_.isNumber(value)) {
+								value = value + 4;
+							} else if (_.isPlainObject(value)) {
+								value.addedThing3 = true;
+							}
+							resolve(value);
+						}, 5);
+					});
+				}
+
+			}).then(function(result) {
+				expect(result).to.deep.equal({
+					'foo': {
+						'bar': 'ca8',
+						'baz': 13,
+						'addedThing': true,
+						'addedThing3': true
+					},
+					'arr': [
+						{
+							'zip': new Date('2014-01-01T00:00:00.000Z'),
+							'addedThing': true,
+							'addedThing3': true
+						},
+						{
+							'zip': new Date('2015-04-02T13:41:08.722Z'),
+							'addedThing': true,
+							'addedThing3': true
+						},
+						{
+							'zip': obj.arr[2].zip,
+							'addedThing': true,
+							'addedThing3': true
+						},
+						{
+							'addedThing': true,
+							'addedThing3': true
+						}
+					],
+					'map': {
+						'foo': 7,
+						'bar': 9,
+						'addedThing': true,
+						'addedThing3': true
+					},
+					'bin': obj.bin,
+					'boo': true,
+					'mix': {
+						'a': [
+							obj.mix.a[0]
+						],
+						'addedThing': true,
+						'addedThing3': true
+					},
+					'o': {
+						'qux': 9,
+						'bam': 'ca7',
+						'addedThing': true,
+						'addedThing3': true
+					},
+					'extraField': 'bfoo',
+					'addedThing': true,
+					'addedThing3': true
+				});
+				done();
+			}).catch(done);
+
 		});
 
 	});
