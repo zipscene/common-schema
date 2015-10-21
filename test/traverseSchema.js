42c256026d704e0f9dbd13dec7f8b559fa90977b
Erin Thompson <erin.thompson@zipscene.com>
2015-10-21 16:16:32 -0400

    updated _traverseSubschema to stop traversing when the handler returns false

--- test/traverseSchema.js
+++ test/traverseSchema.js
@@ -3,7 +3,7 @@ let createSchema = require('../lib').createSchema;
 
 describe('#traverseSchema', function() {
 
-	it('traverse', function() {
+	it('traverse', function(done) {
 
 		let types = [];
 		let paths = [];
@@ -24,7 +24,35 @@ describe('#traverseSchema', function() {
 
 		expect(types).to.deep.equal([ 'object', 'object', 'number', 'string' ]);
 		expect(paths).to.deep.equal([ '', 'foo', 'foo.bar', 'foo.baz' ]);
+		done();
 
+	});
+
+	it('should stop traversing a path if it returns false', function(done) {
+		let types = [];
+		let paths = [];
+
+		let schema = createSchema({
+			foo: [ {
+				bar: Number,
+				baz: String
+			} ],
+			bat: Number
+		});
+
+		schema.traverseSchema({
+			onSubschema(subschema, path) {
+				types.push(subschema.type);
+				paths.push(path);
+				if (subschema.type === 'array') {
+					return false;
+				}
+			}
+		});
+
+		expect(types).to.deep.equal([ 'object', 'array', 'number' ]);
+		expect(paths).to.deep.equal([ '', 'foo', 'bat' ]);
+		done();
 	});
 
 });
